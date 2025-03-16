3620c3bf08b0fdc49ad9fc631ddd31e38717db11
shayancreates <shayansets@gmail.com>
2025-03-17 00:41:20 +0530

    final

---
+++ components/hero.jsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const HeroSection = () => {
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const imageElement = imageRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <section className="pt-40 pb-20 px-4">
+      <div className="container mx-auto text-center">
+        <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
+          Financial Inclusion <br /> with Savebuddy
+        </h1>
+
+        <div className="flex justify-center space-x-4">
+          <Link href="/dashboard">
+            <Button size="lg" className="px-8">
+              Get Started
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
